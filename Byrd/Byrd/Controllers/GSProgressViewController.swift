e5e50821e1b6b2ef95618c7e605e0d0f7fa1d855
lucas.fernandes.a <lucasagostif@gmail.com>
2020-05-07 16:54:19 -0300

    adicionando logo do app e configurando onboarding screen

---
+++ Byrd/Byrd/Controllers/GSProgressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GSProgressViewController.swift
+//  Byrd
+//
+//  Created by Lucas Fernandes on 07/05/20.
+//  Copyright © 2020 Dorime. All rights reserved.
+//
+
+import UIKit
+
+class GSProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Byrd/Byrd/Controllers/GSViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GSViewController.swift
+//  Byrd
+//
+//  Created by Lucas Fernandes on 07/05/20.
+//  Copyright © 2020 Dorime. All rights reserved.
+//
+
+import UIKit
+
+class GSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
